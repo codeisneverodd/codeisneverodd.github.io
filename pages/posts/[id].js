6fb588327613f99b5d4f646203666657b4238ba8
codeisneverodd <codeisneverodd@gmail.com>
2022-11-02 14:13:18 +0900

    fix: Date 경로

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Date from "../../components/Date";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
